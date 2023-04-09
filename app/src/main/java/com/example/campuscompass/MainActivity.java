633aae9aba93e1dfb19870163d1e5ed3de533b50
Prathviraj-Shetty <prathvirajshetty7698@gmail.com>
2023-04-09 15:23:49 +0530

    Home page design

--- app/src/main/java/com/example/campuscompass/MainActivity.java
+++ app/src/main/java/com/example/campuscompass/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.campuscompass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton imgbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgbtn=(ImageButton) findViewById(R.id.imageButton);
+        imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Explore", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
