c8048e6a7933b977b883ddc0fea33e1eb5923164
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-05-08 11:47:36 +0530

    feat: path coloring

--- app/src/main/java/com/example/campuscompass/Floor.java
+++ app/src/main/java/com/example/campuscompass/Floor.java
@@ -19,6 +19,7 @@ public class Floor extends Fragment {
     String []places = {"LH301", "LH302", "BTL10", "BTL07", "BT HOD Cabin", "LH210", "LH211", "LH212", "Dept of Physical Education", "BT Staffroom", "Biokinetics Lab", "Instrumentation and Project Lab"};
     TextView[][] nodes=new TextView[6][8];
     TextView[] mainNodes=new TextView[6];
+    View[] lines = new View[11];
     int[][] ids={
         {R.id.topleftNode0,R.id.topNode0,R.id.toprightNode0,R.id.rightNode0,R.id.bottomrightNode0,R.id.bottomNode0,R.id.bottomleftNode0,R.id.leftNode0},
         {R.id.topleftNode1,R.id.topNode1,R.id.toprightNode1,R.id.rightNode1,R.id.bottomrightNode1,R.id.bottomNode1,R.id.bottomleftNode1,R.id.leftNode1},
@@ -61,12 +62,63 @@ public class Floor extends Fragment {
         mainNodes[4]=v.findViewById(R.id.Node4);
         mainNodes[5]=v.findViewById(R.id.Node5);
 
+        lines[0]=v.findViewById(R.id.line01);
+        lines[1]=v.findViewById(R.id.line12);
+        lines[2]=v.findViewById(R.id.line23);
+        lines[3]=v.findViewById(R.id.line34);
+        lines[4]=v.findViewById(R.id.line50);
+
+        lines[5]=v.findViewById(R.id.line0s);
+        lines[6]=v.findViewById(R.id.line3s);
+        lines[7]=v.findViewById(R.id.linestairs2);
+
+        lines[8]=v.findViewById(R.id.line5s);
+        lines[9]=v.findViewById(R.id.line4s);
+        lines[10]=v.findViewById(R.id.linestairs1);
+
         locations=new Location[]{node0,node1,node2,node3,node4,node5};
         for(int i=0;i<6;i++){
             for(int j=0;j<8;j++){
                 nodes[i][j] = v.findViewById(ids[i][j]);
             }
         }
+
+        if(node0.getInRoute() && node1.getInRoute()){
+            lines[0].setBackgroundColor(Color.rgb(0,255,0));
+        }
+        if(node1.getInRoute() && node2.getInRoute()){
+            lines[1].setBackgroundColor(Color.rgb(0,255,0));
+        }
+        if(node2.getInRoute() && node3.getInRoute()){
+            lines[2].setBackgroundColor(Color.rgb(0,255,0));
+        }
+        if(node3.getInRoute() && node4.getInRoute()){
+            lines[3].setBackgroundColor(Color.rgb(0,255,0));
+        }
+        if(node5.getInRoute() && node0.getInRoute()){
+            lines[4].setBackgroundColor(Color.rgb(0,255,0));
+        }
+
+        if((node0.getInRoute() && stairs1.getInRoute())||(node0.getInRoute() && node3.getInRoute())){
+            lines[5].setBackgroundColor(Color.rgb(0,255,0));
+        }
+        if((node3.getInRoute() && stairs1.getInRoute())||(node0.getInRoute() && node3.getInRoute())){
+            lines[6].setBackgroundColor(Color.rgb(0,255,0));
+        }
+        if((node0.getInRoute() && stairs1.getInRoute())||(node3.getInRoute() && stairs1.getInRoute())){
+            lines[7].setBackgroundColor(Color.rgb(0,255,0));
+        }
+
+        if((node5.getInRoute() && stairs2.getInRoute())||(node5.getInRoute() && node4.getInRoute())){
+            lines[8].setBackgroundColor(Color.rgb(0,255,0));
+        }
+        if((node4.getInRoute() && stairs2.getInRoute())||(node5.getInRoute() && node4.getInRoute())){
+            lines[9].setBackgroundColor(Color.rgb(0,255,0));
+        }
+        if((node5.getInRoute() && stairs2.getInRoute())||(node4.getInRoute() && stairs2.getInRoute())){
+            lines[10].setBackgroundColor(Color.rgb(0,255,0));
+        }
+
 
         for(int i=0;i<6;i++){
             ArrayList<String> places=locations[i].getPlaces();
